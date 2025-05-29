b272b14781bf62cf2c60a94c42869ece98df3e75
Braaiday <pierre.putter@gmail.com>
2025-05-29 22:13:01 +0200

    Adding user indication for the user of the chat you are in.

---
+++ src/components/ChatUsers.tsx
@@ -0,0 +1,63 @@
+import { useAuth } from "../context/AuthProvider";
+import { useChatApp } from "../context/ChatAppProvider";
+import { useUserStatus } from "../hooks/useUserStatus";
+import { StatusColors } from "../types/StatusColors";
+import { Avatar } from "./Avatar";
+import { GroupAvatarCluster } from "./GroupAvatarCluster";
+
+export const ChatUsers = () => {
+  const { user } = useAuth();
+
+  const { selectedChat } = useChatApp();
+
+  const isGroup = selectedChat?.type === "group";
+  const friendId =
+    selectedChat?.participants.find((id) => id !== user?.uid) ?? null;
+
+  const status = useUserStatus(friendId) || "offline";
+
+  return (
+    <>
+      {selectedChat?.participants
+        .filter((participant) => participant !== user?.uid)
+        .map((participant) => {
+          const url = selectedChat.photoURLs[participant];
+          const displayName = selectedChat.friendlyNames[participant];
+          return (
+            <>
+              {!isGroup && (
+                <>
+                  <Avatar url={url} displayName={displayName} />
+
+                  <div className="flex flex-col">
+                    <span className="text-sm font-medium">{displayName}</span>
+                    <div className="flex items-center gap-1">
+                      <span
+                        className={`w-2.5 h-2.5 rounded-full ${
+                          StatusColors[status] || "bg-gray-400"
+                        }`}
+                        aria-label={status}
+                      />
+                      <span className="text-xs text-gray-500 capitalize">
+                        {status}
+                      </span>
+                    </div>
+                  </div>
+                </>
+              )}
+            </>
+          );
+        })}
+
+      {isGroup && (
+        <>
+          <GroupAvatarCluster
+            photoURLs={selectedChat.photoURLs}
+            participants={selectedChat.participants}
+            friendlyNames={selectedChat.friendlyNames}
+          />
+        </>
+      )}
+    </>
+  );
+};
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,11 +1,18 @@
 import { ThemeToggle } from "./ThemeToggle";
 import { AddFriend } from "./AddFriend";
 import { Notifications } from "./Notifications";
+import { ChatUsers } from "./ChatUsers";
 
 export const TopBar = () => {
   return (
     <div className="h-14 bg-card border-b border-border flex items-center px-4">
-      <div className="flex items-center space-x-2 ml-auto">
+      <div className="flex items-center space-x-2">
+        <ChatUsers/>
+      </div>
+
+      <div className="flex-grow" />
+
+      <div className="flex items-center space-x-2">
         <Notifications />
         <AddFriend />
         <ThemeToggle />
